af54b88d82e0eab1acf54cf0a299f8f20804b738
parasharparth <parthparashar6@gmail.com>
2022-05-15 17:39:37 -0700

    Removed comments for new features to simplify the application

--- src/components/Homepage/homepage.jsx
+++ src/components/Homepage/homepage.jsx
@@ -9,9 +9,7 @@ class homepage extends Component{
         }
     }
 
-    //Adding all the events and features to be included on the home page
-    //We will make use of the carousel to beautify the page so that the movie and event lists are displayed properly
-    //The features to be added are: - recommended movies, entertainment, premier movies, outdoorevents, laughter events, popular events, latest events
+    
     render(){
         return (
             <div>

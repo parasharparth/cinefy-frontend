eba9485a4bf34bf7c608bf997ecf0d90d0a17329
kirtan6004 <kirtanpatel209@gmail.com>
2022-05-30 12:57:17 -0700

    booking updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Home from './components/Homepage/Home';
 import Bookings from './components/Bookings/BookingPage';
 import About from './components/AboutUS/About';
-import Payments from './components/PaymentsPage/Payments';
+
 import Nav from './components/Header';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -13,7 +13,7 @@ function App() {
      <Router>
        <Nav/>
       <Switch>
-        <route path="/Payments" component={Payments} />
+
         <Route path="/bookings" component={Bookings} />
         <Route path="/about" component={About} />
         <Route path="/" component={Home} />
